18696ba89fbca5b0f221161a1af343edb8d43a92
sanavinaik10 <67157233+sanavinaik10@users.noreply.github.com>
2020-08-06 19:48:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function draw(){
             document.getElementById("speed").innerHTML = "Speed = 2x";
             song.rate(2); 
         }
-        else{
+        else if(rightWristY > 400 && rightWristY <= 500){
             document.getElementById("speed").innerHTML = "Speed = 2.5x";
             song.rate(2.5); 
         }
